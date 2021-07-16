09b69afbb926ecf3cc059631083738236e8d3515
Takuma Kawamura <account@bit-plus.work>
2021-07-16 16:35:34 +0900

    Support screen dimension

--- SimplifiedEyeTracker.cs
+++ SimplifiedEyeTracker.cs
@@ -94,29 +94,42 @@ namespace SimplifiedEyeTracker
         /// Tobii.Research eye tracker object
         /// </summary>
         private readonly IEyeTracker device;
+        /// <summary>
+        /// Screen Width
+        /// </summary>
+        private readonly double screenWidth;
+        /// <summary>
+        /// Screen Height
+        /// </summary>
+        private readonly double screenHeight;
 
         /// <summary>
         /// Delegate for the event <see cref="E:SimplifiedEyeTracker.EyeTracker.OnGazeData"/>
         /// </summary>
         /// <param name="sender">Event sender</param>
-        /// <param name="e">Event arguments <seealso cref="T:SimplifiedEyeTracker.SimplifiedGazeDataEventArgs"/></param>
+        /// <param name="e">Event arguments <seealso cref="SimplifiedEyeTracker.SimplifiedGazeDataEventArgs"/></param>
         public delegate void SimplifiedGazeDataEventHandler(object sender, SimplifiedGazeDataEventArgs e);
 
         /// <summary>
         /// EventHandler for make gaze data available from user
         /// </summary>
+        /// <remarks>
+        /// If you specify a screen dimension in the constructor, that changes max values for gaze positions.
+        /// </remarks>
         public event SimplifiedGazeDataEventHandler OnGazeData;
 
         /// <summary>
         /// Constructor. Use the first eye tracker in the <c>Tobii.Research.EyeTrackerCollection</c>.
         /// </summary>
-        /// <exception cref="InvalidOperationException"></exception>
+        /// <exception cref="InvalidOperationException">Eye Tracker(s) not found</exception>
         public EyeTracker()
         {
             EyeTrackerCollection eyeTrackers = EyeTrackingOperations.FindAllEyeTrackers();
             if (eyeTrackers.Count > 0)
             {
                 this.device = eyeTrackers[0];
+                this.screenWidth = 0.0;
+                this.screenHeight = 0.0;
             }
             else
             {
@@ -124,11 +137,36 @@ namespace SimplifiedEyeTracker
             }
         }
         /// <summary>
-        /// Overloaded Constructor. Use the eye tracker which has <c>identificationString</c> for <c>identificationType</c> in the <c>Tobii.Research.EyeTrackerCollection</c>.
+        /// Overloaded constructor. Use the first eye tracker in the <c>Tobii.Research.EyeTrackerCollection</c>.
+        /// </summary>
+        /// <param name="screenWidth">Max value of gaze x positions in <see cref="E:SimplifiedEyeTracker.EyeTracker.OnGazeData"/></param>
+        /// <param name="screenHeight">Max value of gaze y positions in <see cref="E:SimplifiedEyeTracker.EyeTracker.OnGazeData"/></param>
+        /// <exception cref="ArgumentOutOfRangeException">Provided screen dimension is invalid</exception>
+        /// <exception cref="InvalidOperationException">Eye tracker(s) not found</exception>
+        public EyeTracker(double screenWidth, double screenHeight)
+        {
+            EyeTrackerCollection eyeTrackers = EyeTrackingOperations.FindAllEyeTrackers();
+            if (screenWidth <= 0 || screenHeight <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Provided screen dimension is invalid.");
+            }            
+            else if (eyeTrackers.Count > 0)
+            {
+                this.device = eyeTrackers[0];
+                this.screenWidth = screenWidth;
+                this.screenHeight = screenHeight;
+            }
+            else
+            {
+                throw new InvalidOperationException("Eye Tracker(s) not found.");
+            }
+        }
+        /// <summary>
+        /// Overloaded constructor. Use the eye tracker which has <c>identificationString</c> for <c>identificationType</c> in the <c>Tobii.Research.EyeTrackerCollection</c>.
         /// </summary>
         /// <param name="identificationType">The way to specify the eye tracker. <seealso cref="T:SimplifiedEyeTracker.EyeTrackerIdentification"/></param>
         /// <param name="identificationString">The string for specify the eye tracker.</param>
-        /// <exception cref="InvalidOperationException"></exception>
+        /// <exception cref="InvalidOperationException">Eye Tracker(s) not found</exception>
         public EyeTracker(EyeTrackerIdentification identificationType, string identificationString)
         {
             EyeTrackerCollection eyeTrackers = EyeTrackingOperations.FindAllEyeTrackers();
@@ -143,13 +181,15 @@ namespace SimplifiedEyeTracker
                             if (eyeTracker.DeviceName == identificationString)
                             {
                                 this.device = eyeTracker;
+                                this.screenWidth = 0.0;
+                                this.screenHeight = 0.0;
                                 initialized = true;
                                 break;
                             }
                         }
                         if (!initialized)
                         {
-                            throw new InvalidOperationException($"Eye Tracker not found at the Device Name: {identificationString}");
+                            throw new InvalidOperationException($"Eye Tracker not found with the Device Name: {identificationString}");
                         }
                         break;
                     case EyeTrackerIdentification.SerialNumber:
@@ -158,13 +198,15 @@ namespace SimplifiedEyeTracker
                             if (eyeTracker.SerialNumber == identificationString)
                             {
                                 this.device = eyeTracker;
+                                this.screenWidth = 0.0;
+                                this.screenHeight = 0.0;
                                 initialized = true;
                                 break;
                             }
                         }
                         if (!initialized)
                         {
-                            throw new InvalidOperationException($"Eye Tracker not found at the Serial Number: {identificationString}");
+                            throw new InvalidOperationException($"Eye Tracker not found with the Serial Number: {identificationString}");
                         }
                         break;
                     case EyeTrackerIdentification.Model:
@@ -173,13 +215,15 @@ namespace SimplifiedEyeTracker
                             if (eyeTracker.Model == identificationString)
                             {
                                 this.device = eyeTracker;
+                                this.screenWidth = 0.0;
+                                this.screenHeight = 0.0;
                                 initialized = true;
                                 break;
                             }
                         }
                         if (!initialized)
                         {
-                            throw new InvalidOperationException($"Eye Tracker not found at the Model: {identificationString}");
+                            throw new InvalidOperationException($"Eye Tracker not found with the Model: {identificationString}");
                         }
                         break;
                     case EyeTrackerIdentification.FirmwareVersion:
@@ -188,13 +232,15 @@ namespace SimplifiedEyeTracker
                             if (eyeTracker.FirmwareVersion == identificationString)
                             {
                                 this.device = eyeTracker;
+                                this.screenWidth = 0.0;
+                                this.screenHeight = 0.0;
                                 initialized = true;
                                 break;
                             }
                         }
                         if (!initialized)
                         {
-                            throw new InvalidOperationException($"Eye Tracker not found at the Firmware Version: {identificationString}");
+                            throw new InvalidOperationException($"Eye Tracker not found with the Firmware Version: {identificationString}");
                         }
                         break;
                     case EyeTrackerIdentification.RuntimeVersion:
@@ -203,13 +249,128 @@ namespace SimplifiedEyeTracker
                             if (eyeTracker.RuntimeVersion == identificationString)
                             {
                                 this.device = eyeTracker;
+                                this.screenWidth = 0.0;
+                                this.screenHeight = 0.0;
                                 initialized = true;
                                 break;
                             }
                         }
                         if (!initialized)
                         {
-                            throw new InvalidOperationException($"Eye Tracker not found at the Runtime Version: {identificationString}");
+                            throw new InvalidOperationException($"Eye Tracker not found with the Runtime Version: {identificationString}");
+                        }
+                        break;
+                }
+            }
+            else
+            {
+                throw new InvalidOperationException("Eye Tracker(s) not found.");
+            }
+        }
+        /// <summary>
+        /// Overloaded constructor. Use the eye tracker which has <c>identificationString</c> for <c>identificationType</c> in the <c>Tobii.Research.EyeTrackerCollection</c>.
+        /// </summary>
+        /// <param name="identificationType">The way to specify the eye tracker. <seealso cref="T:SimplifiedEyeTracker.EyeTrackerIdentification"/></param>
+        /// <param name="identificationString">The string for specify the eye tracker.</param>
+        /// <param name="screenWidth">Max value of gaze x positions in <see cref="E:SimplifiedEyeTracker.EyeTracker.OnGazeData"/></param>
+        /// <param name="screenHeight">Max value of gaze y positions in <see cref="E:SimplifiedEyeTracker.EyeTracker.OnGazeData"/></param>
+        /// <exception cref="ArgumentOutOfRangeException">Provided screen dimension is invalid</exception>
+        /// <exception cref="InvalidOperationException">Eye Tracker(s) not found</exception>
+        public EyeTracker(EyeTrackerIdentification identificationType, string identificationString, double screenWidth, double screenHeight)
+        {
+            EyeTrackerCollection eyeTrackers = EyeTrackingOperations.FindAllEyeTrackers();
+            if (screenWidth <= 0 || screenHeight <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Provided screen dimension is invalid.");
+            }
+            else if (eyeTrackers.Count > 0)
+            {
+                bool initialized = false;
+                switch (identificationType)
+                {
+                    case EyeTrackerIdentification.DeviceName:
+                        foreach (IEyeTracker eyeTracker in eyeTrackers)
+                        {
+                            if (eyeTracker.DeviceName == identificationString)
+                            {
+                                this.device = eyeTracker;
+                                this.screenWidth = screenWidth;
+                                this.screenHeight = screenHeight;
+                                initialized = true;
+                                break;
+                            }
+                        }
+                        if (!initialized)
+                        {
+                            throw new InvalidOperationException($"Eye Tracker not found with the Device Name: {identificationString}");
+                        }
+                        break;
+                    case EyeTrackerIdentification.SerialNumber:
+                        foreach (IEyeTracker eyeTracker in eyeTrackers)
+                        {
+                            if (eyeTracker.SerialNumber == identificationString)
+                            {
+                                this.device = eyeTracker;
+                                this.screenWidth = screenWidth;
+                                this.screenHeight = screenHeight;
+                                initialized = true;
+                                break;
+                            }
+                        }
+                        if (!initialized)
+                        {
+                            throw new InvalidOperationException($"Eye Tracker not found with the Serial Number: {identificationString}");
+                        }
+                        break;
+                    case EyeTrackerIdentification.Model:
+                        foreach (IEyeTracker eyeTracker in eyeTrackers)
+                        {
+                            if (eyeTracker.Model == identificationString)
+                            {
+                                this.device = eyeTracker;
+                                this.screenWidth = screenWidth;
+                                this.screenHeight = screenHeight;
+                                initialized = true;
+                                break;
+                            }
+                        }
+                        if (!initialized)
+                        {
+                            throw new InvalidOperationException($"Eye Tracker not found with the Model: {identificationString}");
+                        }
+                        break;
+                    case EyeTrackerIdentification.FirmwareVersion:
+                        foreach (IEyeTracker eyeTracker in eyeTrackers)
+                        {
+                            if (eyeTracker.FirmwareVersion == identificationString)
+                            {
+                                this.device = eyeTracker;
+                                this.screenWidth = screenWidth;
+                                this.screenHeight = screenHeight;
+                                initialized = true;
+                                break;
+                            }
+                        }
+                        if (!initialized)
+                        {
+                            throw new InvalidOperationException($"Eye Tracker not found with the Firmware Version: {identificationString}");
+                        }
+                        break;
+                    case EyeTrackerIdentification.RuntimeVersion:
+                        foreach (IEyeTracker eyeTracker in eyeTrackers)
+                        {
+                            if (eyeTracker.RuntimeVersion == identificationString)
+                            {
+                                this.device = eyeTracker;
+                                this.screenWidth = screenWidth;
+                                this.screenHeight = screenHeight;
+                                initialized = true;
+                                break;
+                            }
+                        }
+                        if (!initialized)
+                        {
+                            throw new InvalidOperationException($"Eye Tracker not found with the Runtime Version: {identificationString}");
                         }
                         break;
                 }
@@ -264,7 +425,7 @@ namespace SimplifiedEyeTracker
         /// <summary>
         /// Start for receiving gaze data. Before calling it, you must set <see cref="E:SimplifiedEyeTracker.EyeTracker.OnGazeData"/>
         /// </summary>
-        /// <exception cref="InvalidOperationException"></exception>
+        /// <exception cref="InvalidOperationException">EyeTracker.OnGazeData is null</exception>
         public void StartReceivingGazeData()
         {
             if (this.device != null && this.OnGazeData != null)
@@ -273,13 +434,13 @@ namespace SimplifiedEyeTracker
             }
             else
             {
-                throw new InvalidOperationException("EyeTracker.OnGazeData is null. Cannot Start Listening Gaze Data.");
+                throw new InvalidOperationException("EyeTracker.OnGazeData is null. Cannot Start.");
             }
         }
         /// <summary>
         /// Stop for receiving gaze data. Before calling it, you must set <see cref="E:SimplifiedEyeTracker.EyeTracker.OnGazeData"/>
         /// </summary>
-        /// <exception cref="InvalidOperationException"></exception>
+        /// <exception cref="InvalidOperationException">EyeTracker.OnGazeData is null</exception>
         public void StopReceivingGazeData()
         {
             if (this.device != null && this.OnGazeData != null)
@@ -288,7 +449,7 @@ namespace SimplifiedEyeTracker
             }
             else
             {
-                throw new InvalidOperationException("EyeTracker.OnGazeData is null. Cannot Stop Listening Gaze Data.");
+                throw new InvalidOperationException("EyeTracker.OnGazeData is null. Cannot Stop.");
             }
         }
 
@@ -299,18 +460,36 @@ namespace SimplifiedEyeTracker
         /// <param name="e">Event arguments</param>
         private void GazeDataReceived(object sender, GazeDataEventArgs e)
         {
-            SimplifiedGazeDataEventArgs gazeData = new SimplifiedGazeDataEventArgs()
+            if (this.screenWidth <= 0.0 || this.screenHeight <= 0.0)
             {
-                DeviceTimeStamp = e.DeviceTimeStamp,
-                SystemTimeStamp = e.SystemTimeStamp,
-                LeftX = Convert.ToDouble(e.LeftEye.GazePoint.PositionOnDisplayArea.X),
-                RightX = Convert.ToDouble(e.RightEye.GazePoint.PositionOnDisplayArea.X),
-                LeftY = Convert.ToDouble(e.LeftEye.GazePoint.PositionOnDisplayArea.Y),
-                RightY = Convert.ToDouble(e.RightEye.GazePoint.PositionOnDisplayArea.Y),
-                IsLeftValid = e.LeftEye.GazeOrigin.Validity == Validity.Valid,
-                IsRightValid = e.RightEye.GazeOrigin.Validity == Validity.Valid
-            };
-            this.OnGazeData?.Invoke(this, gazeData);
+                SimplifiedGazeDataEventArgs gazeData = new SimplifiedGazeDataEventArgs()
+                {
+                    DeviceTimeStamp = e.DeviceTimeStamp,
+                    SystemTimeStamp = e.SystemTimeStamp,
+                    LeftX = Convert.ToDouble(e.LeftEye.GazePoint.PositionOnDisplayArea.X),
+                    RightX = Convert.ToDouble(e.RightEye.GazePoint.PositionOnDisplayArea.X),
+                    LeftY = Convert.ToDouble(e.LeftEye.GazePoint.PositionOnDisplayArea.Y),
+                    RightY = Convert.ToDouble(e.RightEye.GazePoint.PositionOnDisplayArea.Y),
+                    IsLeftValid = e.LeftEye.GazeOrigin.Validity == Validity.Valid,
+                    IsRightValid = e.RightEye.GazeOrigin.Validity == Validity.Valid
+                };
+                this.OnGazeData?.Invoke(this, gazeData);
+            }
+            else
+            {
+                SimplifiedGazeDataEventArgs gazeDataUsingScreenDimension = new SimplifiedGazeDataEventArgs()
+                {
+                    DeviceTimeStamp = e.DeviceTimeStamp,
+                    SystemTimeStamp = e.SystemTimeStamp,
+                    LeftX = Convert.ToDouble(e.LeftEye.GazePoint.PositionOnDisplayArea.X) * this.screenWidth,
+                    RightX = Convert.ToDouble(e.RightEye.GazePoint.PositionOnDisplayArea.X) * this.screenWidth,
+                    LeftY = Convert.ToDouble(e.LeftEye.GazePoint.PositionOnDisplayArea.Y) * this.screenHeight,
+                    RightY = Convert.ToDouble(e.RightEye.GazePoint.PositionOnDisplayArea.Y) * this.screenHeight,
+                    IsLeftValid = e.LeftEye.GazeOrigin.Validity == Validity.Valid,
+                    IsRightValid = e.RightEye.GazeOrigin.Validity == Validity.Valid
+                };
+                this.OnGazeData?.Invoke(this, gazeDataUsingScreenDimension);
+            }
         }
     }
 }
